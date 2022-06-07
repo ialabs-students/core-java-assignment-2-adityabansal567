158ca94b2409c9811138546a56c5bed7eca6aead
aditya bansal <43151984+adityabansal567@users.noreply.github.com>
2022-06-07 15:15:49 +0530

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -2,24 +2,25 @@ package com.assignment;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class Assignment {
 	
 	public Assignment() {
 		
 	}
-	ArrayList<Integer> SortDesc(ArrayList<Integer> arr){
-		Collections.sort(arr,Collections.reverseOrder());
-		return arr;
+	ArrayList<BigInteger> arrayListSortDesc(ArrayList<BigInteger> arr){
+	Collections.sort(arr,Collections.reverseOrder());
+	return arr;
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		ArrayList<Integer> arr = new ArrayList<Integer>();
+		ArrayList<BigInteger> arr = new ArrayList<BigInteger>();
 		System.out.println("Enter the number of elements");
 		int g = sc.nextInt();
 		for(int i=0;i<g;i++) {
 			System.out.println("Enter numbers");
-			arr.add(sc.nextInt());
+			arr.add(sc.nextBigInteger());
 		}
 		System.out.println("before sorting: ");
 		System.out.println(arr);
